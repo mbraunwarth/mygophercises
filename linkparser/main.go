0d92bfa264c0f34c0d77461beed22399b54456a5
mbraunwarth <17438264+mbraunwarth@users.noreply.github.com>
2021-02-16 18:08:45 +0100

    added flag parsing and created reader

--- linkparser/main.go
+++ linkparser/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+	var (
+		input string
+		usage = "Usage: link -input <PATH_TO_HTML>"
+	)
+
+	flag.StringVar(&input, "input", "", "specify the html input file")
+	flag.Parse()
+
+	// if input file not specified exit with error
+	if input == "" {
+		fmt.Fprintf(os.Stderr, "please specify an HTML document to parse\n%v\n", usage)
+	}
+
+	f, err := os.Open(input)
+	if err != nil {
+		log.Fatalf("could not open file %v: %v", input, err)
+	}
+	defer f.Close()
 }
