6d60ec47bf062786e4916350565e883bffedb2ae
mbraunwarth <17438264+mbraunwarth@users.noreply.github.com>
2021-02-18 17:43:21 +0100

    todos added to sitemap

--- sitemap/sitemap/sitemap.go
+++ sitemap/sitemap/sitemap.go
@@ -1,24 +1,26 @@
 package sitemap
 
 import (
-	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/mbraunwarth/sitemap/link"
 )
 
 const (
 	ns09 = "http://www.sitemaps.org/schemas/sitemap/0.9"
 )
 
-type m map[string]string
+// TODO restructure Sitemap struct
 
 // Sitemap structure.
 type Sitemap struct {
-	m
+	m         map[string]string
 	host      string
 	namespace string
 }
 
+// TODO Builder does not return any errors, think of 'readSite' first
 // Build returns a new empty Sitemap for the given host.
 func Build(host string) (Sitemap, error) {
 	readSite(host)
@@ -29,6 +31,7 @@ func Build(host string) (Sitemap, error) {
 	}, nil
 }
 
+// TODO new name! make it receiver method on Sitemap for access to host, map etc.
 func readSite(host string) error {
 	// initiate client requests for each link on the site
 	resp, err := http.Get(host)
@@ -36,18 +39,32 @@ func readSite(host string) error {
 		return err
 	}
 
-	b, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(b))
+	// remember closing the body
+	b := resp.Body
+	defer b.Close()
+
+	// get all links from the host
+	ls, err := link.Parse(b)
 	if err != nil {
 		return err
 	}
 
-	// get all links from the host
-	// ls, err := link.Parse(r)
-	// if err != nil {
-	// return link.Link{}, err
-	// }
+	for _, l := range ls {
+		log.Println(l.Href)
+		parseLink(l.Href)
+	}
+
 	return nil
+}
+
+// TODO new function necessary? if so, use receiver method instead
+func parseLink(link string) {
+	// is link in domain?
+	//   true if of form /some/link
+	// otherwise match first part against host value
+	//   true if matching performs with true
+
+	// if true add to sitemap list
 }
 
 // ToXML writes the corresponding XML from the sitemap.
